4b989899d7c4ba4373948b747a68bb5daeff1b8d
e-roy <ericroupe@gmail.com>
2022-06-23 16:44:48 -0400

    basic pages

---
+++ packages/frontend/pages/combat/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const CombatPage: NextPage = () => {
+  return (
+    <div>
+      <div>combat page</div>
+    </div>
+  );
+};
+
+export default CombatPage;
