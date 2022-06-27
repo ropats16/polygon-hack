0275c2616877c49d62ae6a8014d80b96df0f400b
e-roy <ericroupe@gmail.com>
2022-06-27 14:43:06 -0400

    added motion fx

---
+++ packages/frontend/components/motion/MotionSlideLeft.tsx
@@ -0,0 +1,37 @@
+import { motion, Variants } from "framer-motion";
+
+const cardVariants: Variants = {
+  offscreen: {
+    x: -200,
+    opacity: 0,
+  },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    rotate: 0,
+    transition: {
+      type: "spring",
+      bounce: 0,
+      duration: 0.8,
+    },
+  },
+};
+
+interface MotionSlideLeftProps {
+  children: React.ReactNode;
+}
+
+export const MotionSlideLeft = ({ children }: MotionSlideLeftProps) => {
+  return (
+    <motion.div
+      className=""
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <motion.div className="" variants={cardVariants}>
+        {children}
+      </motion.div>
+    </motion.div>
+  );
+};
--- packages/frontend/components/sections/landing/Hero.tsx
+++ packages/frontend/components/sections/landing/Hero.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-
+import { MotionFadeIn } from "@/components/motion";
 import { Button } from "@/components/elements";
 
 export const Hero = () => {
@@ -12,40 +12,43 @@ export const Hero = () => {
   return (
     <div className="relative pt-16 pb-32 overflow-hidden bg-gradient-to-t from-purple-900/50">
       <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48" />
-      <div className="relative">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-            <div>
-              <div></div>
-              <div className="mt-6">
-                <h2 className="text-3xl font-extrabold tracking-tight text-slate-100">
-                  Stay on top of customer support
-                </h2>
-                <p className="mt-4 text-lg text-slate-300">
-                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis
-                  bibendum malesuada faucibus lacinia porttitor. Pulvinar
-                  laoreet sagittis viverra duis. In venenatis sem arcu pretium
-                  pharetra at. Lectus viverra dui tellus ornare pharetra.
-                </p>
+      <MotionFadeIn>
+        <div className="relative">
+          <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+            <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+              <div>
+                <div></div>
                 <div className="mt-6">
-                  <Button onClick={() => router.push("/mint")}>
-                    Enter App
-                  </Button>
+                  <h2 className="text-3xl font-extrabold tracking-tight text-slate-100">
+                    Stay on top of customer support
+                  </h2>
+                  <p className="mt-4 text-lg text-slate-300">
+                    Semper curabitur ullamcorper posuere nunc sed. Ornare
+                    iaculis bibendum malesuada faucibus lacinia porttitor.
+                    Pulvinar laoreet sagittis viverra duis. In venenatis sem
+                    arcu pretium pharetra at. Lectus viverra dui tellus ornare
+                    pharetra.
+                  </p>
+                  <div className="mt-6">
+                    <Button onClick={() => router.push("/mint")}>
+                      Enter App
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0">
-            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              <img
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
-                alt="Inbox user interface"
-              />
+            <div className="mt-12 sm:mt-16 lg:mt-0">
+              <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                <img
+                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                  src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
+                  alt="Inbox user interface"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </MotionFadeIn>
     </div>
   );
 };
