7467d3c9a4b7af71b3765edf03403a784ac9fc09
PBillingsby <philbillingsby@gmail.com>
2022-06-28 14:03:00 -0700

    add options for carousel

---
+++ packages/frontend/components/sections/mint/carouselOptions.js
@@ -0,0 +1,68 @@
+export const warriorSelect = {
+  type: "Warrior",
+  cards: [
+    {
+      name: "Samurai",
+      img: "https://www.historyhit.com/app/uploads/2020/07/Samurai_with_sword-2-e1582108817666-1.jpg?x36313",
+      lore: "viverra dui tellus ornare pharetra. viverra dui"
+    },
+    {
+      name: "Wizard",
+      img: "https://cdn.pixabay.com/photo/2019/08/22/10/29/fantasy-4423131_960_720.jpg",
+      lore: "viverra dui tellus ornare pharetra. viverra dui"
+    },
+    {
+      name: "Knight",
+      img: "https://www.ancient-origins.net/sites/default/files/field/image/Black-Knights-in-Medieval-Legends.jpg",
+      lore: "viverra dui tellus ornare pharetra. viverra dui"
+    }
+
+  ]
+}
+
+export const elementSelect = {
+  type: "Element",
+  cards: [
+    {
+      name: "Fire",
+      img: "https://images.gemexi.com/blog-pictures/need-enthusiasm-in-life-access-the-power-of-the-fire-element-learn-how-picture.jpg",
+      lore: "viverra dui tellus ornare pharetra."
+    },
+    {
+      name: "Earth",
+      img: "https://us.123rf.com/450wm/nuttawut2981/nuttawut29811706/nuttawut2981170600007/79410680-black-and-white-image-of-background-of-dry-cracked-soil-dirt-or-earth-during-drought.jpg",
+      lore: "viverra dui tellus ornare pharetra. viverra dui"
+    },
+    {
+      name: "Air",
+      img: "https://wallpaperaccess.com/full/567845.png",
+      lore: "viverra dui tellus ornare pharetra. viverra dui"
+    },
+    {
+      name: "Water",
+      img: "https://images.fineartamerica.com/images-medium-large/water-splash-in-black-and-white-kathy-clark.jpg",
+      lore: "viverra dui tellus ornare pharetra."
+    }]
+}
+
+export const weaponSelect = {
+  type: "Weapon",
+  cards: [
+    {
+      name: "Samurai",
+      img: "https://c4.wallpaperflare.com/wallpaper/104/564/872/katana-samurai-sword-monochrome-wallpaper-preview.jpg",
+      lore: "viverra dui tellus ornare pharetra."
+    },
+    {
+      name: "Knight",
+      img: "https://img-new.cgtrader.com/items/2185107/5e00fdbdf9/viking-medieval-double-bladed-two-handed-battle-axe-3d-model-low-poly-obj-fbx-dae.jpg",
+      lore: "viverra dui tellus ornare pharetra. viverra dui"
+    },
+    {
+      name: "Magic",
+      img: "https://katrinkaabroad.com/wp-content/uploads/2014/04/si-5761.jpg",
+      lore: "viverra dui tellus ornare pharetra. viverra dui"
+    }
+  ]
+}
+
